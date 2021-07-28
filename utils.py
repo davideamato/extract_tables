4fca863293e14ad7c844a0b236ded454f9458ee2
Hui Ling <huilingw312@gmail.com>
2021-07-28 10:59:12 +0100

    Fix broken table now solves for other cases

--- utils.py
+++ utils.py
@@ -17,6 +17,8 @@ class InputError(Error):
 import os
 import tabula
 
+from collections import Counter
+
 def get_all_files_in_dir(abs_path):
 
     if not os.path.isabs(abs_path):
@@ -24,18 +26,40 @@ def get_all_files_in_dir(abs_path):
 
     return [os.path.join(abs_path, file) for file in os.listdir(abs_path) if file.endswith(".pdf")]
 
-def check_broken_table(current_page_number, filename):
+def check_broken_table(current_page_number, filename, current_table):
 
     tables = tabula.read_pdf(filename, pages=str(current_page_number + 1), lattice=True, guess=True, pandas_options={"header": 0},)
 
+    print("{} {}".format(len(tables[0].columns), len(current_table.columns)))
+
     if tables[0].empty:
         return tables[0].columns.values
+    elif len(tables[0].columns) == len(current_table.columns):
+        tables[0].columns = current_table.columns
+        return tables[0]
     else:
         return None
 
 
 def fix_broken_table(current_page_number, current_table, filename):
-    continued_values = check_broken_table(current_page_number, filename)
+
+    continued_values = check_broken_table(current_page_number, filename, current_table)
+
     if continued_values is not None:
         #add new row to end of DataFrame
-        current_table.loc[len(current_table.index)] = continued_values
+        # current_table.loc[len(current_table.index)] = continued_values
+        updated_table = current_table.append(continued_values, ignore_index=True)
+
+        return updated_table
+    else:
+        return current_table
+
+def desired_tables():
+    acheived_headers = ['Date', 'Body', 'Exam', 'Subject', 'Grade', 'Result', 'Centre Number']
+    achieved_counter = Counter(acheived_headers)
+    predicted_headers = ['Date', 'Body', 'Exam', 'Subject', 'Grade', 'Result', 'Centre\rNumber', 'Predicted\rGrade']
+    predicted_counter = Counter(predicted_headers)
+    examresults_headers = ['Date', 'Body', 'Exam Level', 'Sitting', 'Subject', 'Grade']
+    examresults_counter = Counter(examresults_headers)
+
+    return (achieved_counter, predicted_counter, examresults_counter)
