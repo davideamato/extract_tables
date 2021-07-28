c0247ac8b7a230b61890d51485c705243c67d749
Hui Ling <huilingw312@gmail.com>
2021-07-28 12:07:17 +0100

    Store each of the student in list

--- extract_table.py
+++ extract_table.py
@@ -7,12 +7,13 @@ import os
 import tabula
 
 from utils import desired_tables, get_all_files_in_dir, fix_broken_table, get_applicant_ids
+from student import StudentGrades
 
 path_to_files = os.path.abspath("pdfs/")
 
 all_files = get_all_files_in_dir(path_to_files)
 applicant_ids = get_applicant_ids(path_to_files)
-print(applicant_ids)
+# print(applicant_ids)
 
 TARGET_TABLES = desired_tables()
 EXIT_STRING = 'Type of school, college or training centre:'
@@ -26,13 +27,12 @@ EXIT_STRING = 'Type of school, college or training centre:'
 #     #     print("EXIT CONDITION")
 #     print("")
 
-
-file = all_files[0]
-app_id = applicant_ids[0]
+all_students = [0]*len(all_files)
+counter = 0
 
 for file, app_id in zip(all_files, applicant_ids):
 
-    print(app_id)
+    # print(app_id)
 
     page_number = 2
     exit_loop = False
@@ -62,9 +62,11 @@ for file, app_id in zip(all_files, applicant_ids):
                 exit_loop = True 
 
         if exit_loop:
+            all_students[counter] = StudentGrades(app_id, grade_tables, grade_counters)
             break
 
         page_number += 1
 
+    counter += 1
 
 
