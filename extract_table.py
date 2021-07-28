77dbf698f57feed211f7edbd1247e0f6142facbe
Hui Ling <huilingw312@gmail.com>
2021-07-28 11:41:08 +0100

    Add list to store info

--- extract_table.py
+++ extract_table.py
@@ -6,13 +6,16 @@ import os
 
 from collections import Counter
 
-from utils import desired_tables, get_all_files_in_dir, fix_broken_table
+from utils import desired_tables, get_all_files_in_dir, fix_broken_table, get_applicant_ids
 
+path_to_files = os.path.abspath("pdfs/")
 
-all_files = get_all_files_in_dir(os.path.abspath("pdfs/"))
-# print(all_files)
+all_files = get_all_files_in_dir(path_to_files)
+applicant_ids = get_applicant_ids(path_to_files)
+print(applicant_ids)
 
 file = all_files[0]
+id = applicant_ids[0]
 
 # tables = tabula.read_pdf(file, pages="all", lattice=True, guess=True, pandas_options={"header": 0},)
 
@@ -31,6 +34,9 @@ exit_string = 'Type of school, college or training centre:'
 page_number = 2
 exit_loop = False
 
+grade_tables = []
+grade_counters = []
+
 while True:
     try:
         tables = tabula.read_pdf(file, pages=str(page_number), lattice=True, guess=True, pandas_options={"header": 0},)
@@ -44,6 +50,8 @@ while True:
         header_counter = Counter(list(table_headers.values))
         if header_counter in target_tables:
             table = fix_broken_table(page_number, table, file)
+            grade_tables.append(table)
+            grade_counters.append(header_counter)
 
             print(table)
             print("")
