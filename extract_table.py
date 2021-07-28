6ae62710518fcdb7b160ee763f01b15a22624fb0
Hui Ling <huilingw312@gmail.com>
2021-07-28 11:56:31 +0100

    Iterate over all files in folder

--- extract_table.py
+++ extract_table.py
@@ -1,10 +1,10 @@
 
-from logging import exception
 import subprocess
-import tabula
-import os
 
 from collections import Counter
+
+import os
+import tabula
 
 from utils import desired_tables, get_all_files_in_dir, fix_broken_table, get_applicant_ids
 
@@ -14,11 +14,10 @@ all_files = get_all_files_in_dir(path_to_files)
 applicant_ids = get_applicant_ids(path_to_files)
 print(applicant_ids)
 
-file = all_files[0]
-id = applicant_ids[0]
+TARGET_TABLES = desired_tables()
+EXIT_STRING = 'Type of school, college or training centre:'
 
 # tables = tabula.read_pdf(file, pages="all", lattice=True, guess=True, pandas_options={"header": 0},)
-
 # for table in tables:
 #     print(table)
 #     print(table.columns)
@@ -28,42 +27,44 @@ id = applicant_ids[0]
 #     print("")
 
 
-target_tables = desired_tables()
-exit_string = 'Type of school, college or training centre:'
+file = all_files[0]
+app_id = applicant_ids[0]
 
-page_number = 2
-exit_loop = False
+for file, app_id in zip(all_files, applicant_ids):
 
-grade_tables = []
-grade_counters = []
+    print(app_id)
 
-while True:
-    try:
-        tables = tabula.read_pdf(file, pages=str(page_number), lattice=True, guess=True, pandas_options={"header": 0},)
-    except subprocess.CalledProcessError:
-        break
+    page_number = 2
+    exit_loop = False
 
-    for table in tables:
+    grade_tables = []
+    grade_counters = []
 
-        table_headers = table.columns
+    while True:
+        try:
+            tables = tabula.read_pdf(file, pages=str(page_number), lattice=True, guess=True, pandas_options={"header": 0},)
+        except subprocess.CalledProcessError:
+            break
 
-        header_counter = Counter(list(table_headers.values))
-        if header_counter in target_tables:
-            table = fix_broken_table(page_number, table, file)
-            grade_tables.append(table)
-            grade_counters.append(header_counter)
+        for table in tables:
 
-            print(table)
-            print("")
-        elif exit_string in table_headers:
-            exit_loop = True 
+            table_headers = table.columns
+            header_counter = Counter(list(table_headers.values))
 
+            if header_counter in TARGET_TABLES:
+                table = fix_broken_table(page_number, table, file)
+                grade_tables.append(table)
+                grade_counters.append(header_counter)
 
-    if exit_loop:
-        break
+                print(table)
+                print("")
+            elif EXIT_STRING in table_headers:
+                exit_loop = True 
 
+        if exit_loop:
+            break
 
-    page_number += 1
+        page_number += 1
 
 
 
