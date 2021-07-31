594697838fe6446d6790cc62257afced9e7f55f0
Hui Ling <huilingw312@gmail.com>
2021-07-31 12:48:36 +0100

    Added more permutations

--- utils.py
+++ utils.py
@@ -3,9 +3,11 @@ import tabula
 
 from collections import Counter
 
+
 class Error(Exception):
     """Base class for exceptions in this module."""
     pass
+
 
 class InputError(Error):
     """Exception raised for errors in the input.
@@ -19,25 +21,29 @@ class InputError(Error):
         self.expression = expression
         self.message = message
 
+
 def is_file_valid(file):
     return file.endswith(".pdf") and "unicode" in file
 
+
 def is_abs_path(input_path):
     if not os.path.isabs(input_path):
-        raise InputError(os.path.isabs(input_path), "Path provided is not absolute")
-    
+        raise InputError(os.path.isabs(input_path),
+                         "Path provided is not absolute")
+
     return True
+
 
 def get_all_files_in_dir(abs_path):
 
     if is_abs_path(abs_path):
         return [os.path.join(abs_path, file) for file in os.listdir(abs_path) if is_file_valid(file)]
 
+
 def get_applicant_ids(abs_path):
 
     if is_abs_path(abs_path):
         return [file.split("_")[2] for file in os.listdir(abs_path) if is_file_valid(file)]
-        
 
 
 def check_broken_table(current_page_number, filename, current_table):
@@ -47,7 +53,8 @@ def check_broken_table(current_page_number, filename, current_table):
     '''
 
     # Extract tables from next page
-    tables = tabula.read_pdf(filename, pages=str(current_page_number + 1), lattice=True, guess=True, pandas_options={"header": 0},)
+    tables = tabula.read_pdf(filename, pages=str(
+        current_page_number + 1), lattice=True, guess=True, pandas_options={"header": 0},)
 
     if not tables:
         return None
@@ -67,27 +74,35 @@ def check_broken_table(current_page_number, filename, current_table):
 
 def fix_broken_table(current_page_number, current_table, filename):
 
-    continued_values = check_broken_table(current_page_number, filename, current_table)
+    continued_values = check_broken_table(
+        current_page_number, filename, current_table)
 
     if continued_values is not None:
-        #add new row to end of DataFrame
+        # add new row to end of DataFrame
         # current_table.loc[len(current_table.index)] = continued_values
-        updated_table = current_table.append(continued_values, ignore_index=True)
+        updated_table = current_table.append(
+            continued_values, ignore_index=True)
 
         return updated_table
     else:
         return current_table
 
+
 def raw_table_headers():
-    acheived_headers = ['Date', 'Body', 'Exam', 'Subject', 'Grade', 'Result', 'Centre Number']
-    predicted_headers = ['Date', 'Body', 'Exam', 'Subject', 'Grade', 'Result', 'Centre\rNumber', 'Predicted\rGrade']
-    examresults_headers = ['Date', 'Body', 'Exam Level', 'Sitting', 'Subject', 'Grade']
+    acheived_headers = ['Date', 'Body', 'Exam',
+                        'Subject', 'Grade', 'Result', 'Centre Number']
+    predicted_headers = ['Date', 'Body', 'Exam', 'Subject',
+                         'Grade', 'Result', 'Centre\rNumber', 'Predicted\rGrade']
+    examresults_headers = ['Date', 'Body',
+                           'Exam Level', 'Sitting', 'Subject', 'Grade']
 
     return (acheived_headers, predicted_headers, examresults_headers)
+
 
 def escape_backslash_r(input_string):
     if input_string is not None:
         return input_string.encode('unicode-escape').decode().replace("\\r", " ")
+
 
 def desired_tables():
 
@@ -98,40 +113,108 @@ def desired_tables():
 
     return (achieved_counter, predicted_counter, examresults_counter)
 
+
 def completed_qualification_valid_exams():
-    return ("GCE Advanced\rLevel", "USA-Advanced\rPlacement Test", 
-             "SQA Advanced\rHighers", 
-             "Cambridge Pre-\rU Certificate\r(Principal\rSub",
-             "Matura-\rPoland")
-            #  "GCE Advanced\rSubsidiary", 
-            #  "USA - SAT\r(redesigned\rfrom 2016)", "USA-SAT\rSubject" )
+    return ("GCE Advanced\rLevel",
+            "Cambridge Pre-\rU Certificate\r(Principal\rSub",
+            "Cambridge\rPre-U\rCertificate\r(Principal\rSubject)",
+            "Pearson\rEdexcel\rInternational\rAdvanced\rLevel",
+            "Singapore-\rIntegrated\rProgramme-\rCambridge\rGCE\rAdvanced\rLevel"
+            "SQA Advanced\rHighers",
+            "Spain-Titulo\rde Bachiller",
+            "USA-Advanced\rPlacement Test",
+            "International\rBaccalaureate\rDiploma",
+            "Matura-\rPoland"
+            "France-\rBaccalaureat",
+            "France-\rOption\rInternationale\rdu\rBaccalaureat",
+            "France -\rBaccalaureat\rGeneral (from\r2021)",
+            "France\r-Baccalaureat",
+            "Irish leaving\rcertificate -\rHigher level\r(first awarded\r2017)"
+            )
+    #  "GCE Advanced\rSubsidiary",
+    #  "USA - SAT\r(redesigned\rfrom 2016)", "USA-SAT\rSubject" )
+
 
 def exam_results_valid_exams():
-    return ("SQA Advanced\rHighers", "Pre-U Certificate", "Reformed A Level\rEngland",
-            "GCE A Level (H2)", "GCE A Level (H1)"
+    return (
+        "Reformed A Level\rEngland",
+        "SQA Advanced\rHighers",
+        "Pre-U Certificate",
+        "GCE A Level (H2)",
+        "GCE A Level (H1)",
+        "Cambridge\rInternational A\rLevel",
+        "IB",
+        "IB Standard Level",
+        "Int. Baccalaureate",
+        "IB Total points"
     )
 
+
 def predicted_qualification_valid_exams():
-    return ("Spain-Titulo\rde Bachiller", "Matura-\rPoland", "GCE\rAdvanced\rLevel",
-             "Romania-\rDiploma de\rBacalaureat", 
-             "France -\rBaccalaureat\rGeneral (from\r2021)", 
-             "Singapore-\rIntegrated\rProgramme-\rCambridge\rGCE\rAdvanced\rLevel")
+    return (
+        "GCE\rAdvanced\rLevel",
+        "Cambridge\rPre-U\rCertificate\r(Principal\rSubject)",
+        "Cambridge\rPre-U\rCertificate\r(Principal\rSubject)",
+        "Pearson\rEdexcel\rInternational\rAdvanced\rLevel",
+        "Singapore-\rIntegrated\rProgramme-\rCambridge\rGCE\rAdvanced\rLevel"
+        "SQA Scottish\rHighers",
+        "France -\rBaccalaureat\rGeneral (from\r2021)",
+        "France\r-Baccalaureat",
+        "International\rBaccalaureate\rDiploma",
+        "Spain-Titulo\rde Bachiller",
+        "Matura-\rPoland",
+        "Romania-\rDiploma de\rBacalaureat",
+        "India-Indian\rSchool\rCertificate\r(ISC)",
+        "ISC",
+        "Irish leaving\rcertificate -\rHigher level\r(first awarded\r2017)"
+    )
+
 
 def detail_string():
     return "Module Details/Unit Grades"
 
+
 def predicted_math():
-    return {"GCE Advanced Level": ["Mathematics", "Mathematics (MEI)"],
-            "Singapore-\rIntegrated\rProgramme-\rCambridge\rGCE\rAdvanced\rLevel": ["Mathematics"],
+    return {"GCE Advanced Level": ["Mathematics", "Mathematics (MEI)", "Mathematics A"],
+            "SQA Scottish Highers": ["Mathematics C847", "Mathematics"],
+            "Pearson Edexcel International Advanced Level": ["Mathematics"],
+            "USA-Advanced Placement Test": ["AP Calculus BC",
+                                            "AP Calculus\rBC"
+                                            "CALCULUS BC"
+                                            ],
+            "IB": ["Math Analysis & Appr"],
+            "Int. Baccalaureate": ["Math Analysis & Appr"],
+            "International Baccalaureate Diploma": ["Math Analysis & Appr"],
+            "Cambridge International A Level": ["Mathematics"],
+            "Singapore- Integrated Programme- Cambridge GCE Advanced Level": ["Mathematics"],
+            "Cambridge Pre-U Certificate (Principal Subject)": ["Mathematics (principal subject)"],
             }
 
+
 def predicted_further_math():
-    return {"GCE Advanced Level": ["Further Mathematics (MEI)"],
-            "Singapore-\rIntegrated\rProgramme-\rCambridge\rGCE\rAdvanced\rLevel": ["Further\rMathematics"],
-    
-    }
+    return {"GCE Advanced Level": ["Further Mathematics (MEI)", "Further Mathematics"],
+            "Cambridge International A Level": ["Further Mathematics"],
+            "Pearson Edexcel International Advanced Level": ["Further Mathematics"],
+            "Singapore- Integrated Programme- Cambridge GCE Advanced Level": ["Further Mathematics"],
+            "Cambridge Pre-U Certificate (Principal Subject)": ["Further Mathematics (principal subject)"],
+
+            }
+
 
 def predicted_physics():
     return {"GCE Advanced Level": ["Physics A", "Physics"],
-            "Singapore-\rIntegrated\rProgramme-\rCambridge\rGCE\rAdvanced\rLevel": ["Physics"],
-    }
+            "SQA Scottish Highers": ["Physics C857", "Physics"],
+            "Cambridge International A Level": ["Physics"],
+            "Cambridge Pre-U Certificate (Principal Subject)": ["Physics (principal subject)"],
+            "Pearson Edexcel International Advanced Level": ["Physics"],
+            "IB": ["Physics"],
+            "Int. Baccalaureate": ["Physics"],
+            "International Baccalaureate\rDiploma": ["Physics"],
+            "USA-Advanced Placement Test": ["AP Physics C: Electricity and Magnetism",
+                                            "AP Physics C: Mechanics",
+                                            "AP Physics 1",
+                                            "AP Physics C ELECTRICITY AND MAGNETISM",
+                                            "AP Physics C MECHANICS"
+                                            ],
+            "Singapore- Integrated Programme- Cambridge GCE Advanced Level": ["Physics"],
+            }
