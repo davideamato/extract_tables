f5d932cc2a9e6ae6edbc336ba129d736d8022d7a
Hui Ling <huilingw312@gmail.com>
2021-09-24 12:34:23 +0100

    Added preamble to set up dat

--- testing.py
+++ testing.py
@@ -1,58 +1,91 @@
+import os
 import unittest
+
+import settings
+from utils import get_files_and_ids
 
 
 class TestIDCorrespondence(unittest.TestCase):
-    def banner_target_no_database_not_cumulative(self, ids_from_pdf):
+    def banner_target_no_database_not_cumulative(self):
         # suceeds
+        path_to_files = os.path.join(".", "a")
+        all_files, applicant_ids = get_files_and_ids(path_to_files)
         pass
 
-    def banner_target_no_database_is_cumulative(self, ids_from_pdf):
+    def banner_target_no_database_is_cumulative(self):
         # determine behaviour
+        path_to_files = os.path.join(".", "a")
+        all_files, applicant_ids = get_files_and_ids(path_to_files)
         pass
 
-    def banner_target_with_database_not_cumulative(self, ids_from_pdf):
+    def banner_target_with_database_not_cumulative(self):
         # suceeds
+        path_to_files = os.path.join(".", "a")
+        all_files, applicant_ids = get_files_and_ids(path_to_files)
         pass
 
-    def banner_target_with_database_is_cumulative(self, ids_from_pdf):
+    def banner_target_with_database_is_cumulative(self):
         # suceeds
+        path_to_files = os.path.join(".", "a")
+        all_files, applicant_ids = get_files_and_ids(path_to_files)
         pass
 
-    def banner_target_with_database_not_cumulative_fails(self, ids_from_pdf):
+    def banner_target_with_database_not_cumulative_fails(self):
         # failure situation
+        path_to_files = os.path.join(".", "a")
+        all_files, applicant_ids = get_files_and_ids(path_to_files)
         pass
 
-    def banner_target_with_database_is_cumulative_fails(self, ids_from_pdf):
+    def banner_target_with_database_is_cumulative_fails(self):
         # failure situation
+        path_to_files = os.path.join(".", "a")
+        all_files, applicant_ids = get_files_and_ids(path_to_files)
         pass
 
     def id_only_target_succeeds(self):
+        all_files, applicant_ids = get_files_and_ids(path_to_files)
+        path_to_files = os.path.join(".", "a")
         pass
+
 
 class TestFileIDCorrespondence(unittest.TestCase):
-    def banner_target_no_database_not_cumulative(self, ids_from_pdf):
+    def banner_target_no_database_not_cumulative(self):
         # suceeds
+        path_to_files = os.path.join(".", "a")
+        all_files, applicant_ids = get_files_and_ids(path_to_files)
         pass
 
-    def banner_target_no_database_is_cumulative(self, ids_from_pdf):
+    def banner_target_no_database_is_cumulative(self):
         # determine behaviour
+        path_to_files = os.path.join(".", "a")
+        all_files, applicant_ids = get_files_and_ids(path_to_files)
         pass
 
-    def banner_target_with_database_not_cumulative(self, ids_from_pdf):
+    def banner_target_with_database_not_cumulative(self):
         # suceeds
+        path_to_files = os.path.join(".", "a")
+        all_files, applicant_ids = get_files_and_ids(path_to_files)
         pass
 
-    def banner_target_with_database_is_cumulative(self, ids_from_pdf):
+    def banner_target_with_database_is_cumulative(self):
         # suceeds
+        path_to_files = os.path.join(".", "a")
+        all_files, applicant_ids = get_files_and_ids(path_to_files)
         pass
 
-    def banner_target_with_database_not_cumulative_fails(self, ids_from_pdf):
+    def banner_target_with_database_not_cumulative_fails(self):
         # failure situation
+        path_to_files = os.path.join(".", "a")
+        all_files, applicant_ids = get_files_and_ids(path_to_files)
         pass
 
-    def banner_target_with_database_is_cumulative_fails(self, ids_from_pdf):
+    def banner_target_with_database_is_cumulative_fails(self):
         # failure situation
+        path_to_files = os.path.join(".", "a")
+        all_files, applicant_ids = get_files_and_ids(path_to_files)
         pass
 
     def id_only_target_succeeds(self):
+        path_to_files = os.path.join(".", "a")
+        all_files, applicant_ids = get_files_and_ids(path_to_files)
         pass
