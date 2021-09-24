d0325046c71acd5d65aa0cab8742ad18914aca9d
Hui Ling <huilingw312@gmail.com>
2021-09-24 12:30:41 +0100

    Cases I think that need to be tested

--- testing.py
+++ testing.py
@@ -1,3 +1,58 @@
 import unittest
 
 
+class TestIDCorrespondence(unittest.TestCase):
+    def banner_target_no_database_not_cumulative(self, ids_from_pdf):
+        # suceeds
+        pass
+
+    def banner_target_no_database_is_cumulative(self, ids_from_pdf):
+        # determine behaviour
+        pass
+
+    def banner_target_with_database_not_cumulative(self, ids_from_pdf):
+        # suceeds
+        pass
+
+    def banner_target_with_database_is_cumulative(self, ids_from_pdf):
+        # suceeds
+        pass
+
+    def banner_target_with_database_not_cumulative_fails(self, ids_from_pdf):
+        # failure situation
+        pass
+
+    def banner_target_with_database_is_cumulative_fails(self, ids_from_pdf):
+        # failure situation
+        pass
+
+    def id_only_target_succeeds(self):
+        pass
+
+class TestFileIDCorrespondence(unittest.TestCase):
+    def banner_target_no_database_not_cumulative(self, ids_from_pdf):
+        # suceeds
+        pass
+
+    def banner_target_no_database_is_cumulative(self, ids_from_pdf):
+        # determine behaviour
+        pass
+
+    def banner_target_with_database_not_cumulative(self, ids_from_pdf):
+        # suceeds
+        pass
+
+    def banner_target_with_database_is_cumulative(self, ids_from_pdf):
+        # suceeds
+        pass
+
+    def banner_target_with_database_not_cumulative_fails(self, ids_from_pdf):
+        # failure situation
+        pass
+
+    def banner_target_with_database_is_cumulative_fails(self, ids_from_pdf):
+        # failure situation
+        pass
+
+    def id_only_target_succeeds(self):
+        pass
